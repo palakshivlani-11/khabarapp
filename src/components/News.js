e66e91ccd01abd748db80457e34391d654f1b72a
Palak Shivlani <58842198+palakshivlani-11@users.noreply.github.com>
2023-01-21 07:15:51 -0800

    Added next and prev and some part of loader

--- src/components/News.js
+++ src/components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
+import Spinner from './Spinner';
 
 export class News extends Component {
   articles = [
@@ -268,21 +269,72 @@ export class News extends Component {
     super();
     console.log("Hello I am a constructor from news component");
     this.state = {
-      articles: this.articles,
-      loading : false
+      articles: [],
+      loading : false,
+      page:1
     }
   }
+
+  async componentDidMount()
+  {
+    console.log("cdm");
+    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=a292951ef1a94cdd871951fbd23bc74d&page=1&pageSize=${this.props.pageSize}`;
+    let data = await fetch(url);
+    let parseddata = await data.json()
+    console.log(parseddata);
+    this.setState({articles: parseddata.articles , totalResults: parseddata.totalResults})
+  }
+
+  handleNextClick = async()=>{
+
+    if(this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize))
+    {
+
+    }
+    else {
+    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=a292951ef1a94cdd871951fbd23bc74d&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+    this.setState({loader: true});
+    let data = await fetch(url);
+    let parseddata = await data.json()
+    this.setState({
+      page: this.state.page + 1,
+      articles: parseddata.articles,
+      loader: false
+    })
+  }
+  }
+
+  handlePrevClick = async ()=>{
+   
+    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=a292951ef1a94cdd871951fbd23bc74d&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+    this.setState({loader: true});
+    let data = await fetch(url);
+    let parseddata = await data.json()
+    console.log(parseddata);
+    this.setState({
+      page: this.state.page - 1,
+      articles: parseddata.articles,
+      loader: false
+    })
+  }
+
   render() {
     return (
       <div className='container my-3'>
-        <h2>KhabarApp - Top Headlines</h2>
+        <h1 className="text-center">KhabarApp - Top Headlines</h1>
+        {this.state.loading && <Spinner/>}
         <div className='row'>
         {this.state.articles.map((element)=>{
           return <div className='col-md-4' key={element.url}>
-          <NewsItem  title={element.title.slice(0,63)} description={element.description.slice(0,106)} imageUrl={element.urlToImage} newsUrl={element.url}/>   
+          <NewsItem  title={element.title?element.title.slice(0,63):""} description={element.description?element.description.slice(0,106):""} imageUrl={element.urlToImage} newsUrl={element.url}/>   
         </div>
 })}
-        </div>     
+        </div> 
+        <div className="container d-flex justify-content-between">
+        <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
+        <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+
+          </div>    
       </div>
     )
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,11 +7,11 @@ export class NewsItem extends Component {
     return (
       <div className='my-3'>
        <div className="card" style={{width: "18rem"}}>
-                <img src={imageUrl} className="card-img-top" alt="..."/>
+                <img src={imageUrl?imageUrl:"https://thumbs.dreamstime.com/b/news-newspapers-folded-stacked-word-wooden-block-puzzle-dice-concept-newspaper-media-press-release-42301371.jpg"} className="card-img-top" alt="..."/>
                  <div className="card-body">
                 <h5 className="card-title">{title}...</h5>
                 <p className="card-text">{description}...</p>
-                <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
+                <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
             </div>
             </div>
       </div>
